62788161d167488c44f29664cdbfe1cda352eaad
= <=>
2020-10-02 02:10:36 +0530

    Added DB Creds in .env
    - Added python-dotenv and mysql-connector in project

--- app/main/config.py
+++ app/main/config.py
@@ -4,6 +4,9 @@
     Configuration File for Our Project
 """
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -13,6 +16,8 @@ class Config:
     """
     PROF_NAME = "base"
     DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
 
 
 class DevelopmentConfig(Config):
@@ -20,13 +25,17 @@ class DevelopmentConfig(Config):
     """
     DEBUG = True
     PROF_NAME = "development"
+    SQLALCHEMY_DATABASE_URI = os.environ['DEV_DB_URL']
 
 
 class TestingConfig(Config):
     """ Configuration for Testing Environment
     """
+    TESTING = True
     DEBUG = True
     PROF_NAME = "testing"
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_DATABASE_URI = os.environ['TEST_DB_URL']
 
 
 class ProductionConfig(Config):
@@ -34,6 +43,8 @@ class ProductionConfig(Config):
     """
     DEBUG = False
     PROF_NAME = "production"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = os.environ['PROD_DB_URL']
 
 
 config_by_name = dict(
