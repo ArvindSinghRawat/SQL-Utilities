d587b07137a910043d637af4e129f036b4c9a0ea
= <=>
2020-10-03 02:23:54 +0530

    Refactored Code
    - Added Pattern Validation in DTOs
    - Removed Redundant API Docs

--- app/main/controller/user_controller.py
+++ app/main/controller/user_controller.py
@@ -16,43 +16,41 @@ from ..util.dto import UserDto
 from ..service.user_service import save_new_user, get_all_users, get_a_user
 
 api = UserDto.api
+_user_input = UserDto.user_input
 _user = UserDto.user
 
 
-@api.route('/')
+@api.route("/")
 class UserList(Resource):
     """Used for Users related API"""
-    @api.doc('list_of_registered_users')
-    @api.marshal_list_with(_user, envelope='data')
+    @api.marshal_list_with(_user, envelope="users")
     def get(self):
-        """List all registered users"""
+        """Lists all registered users"""
         return get_all_users()
 
-    @api.response(201, 'User successfully created.')
-    @api.doc('create a new user')
-    @api.expect(_user, validate=True)
+    @api.response(201, "User successfully created.")
+    @api.expect(_user_input, validate=True)
     def post(self):
         """Creates a new User """
         data = request.json
         return save_new_user(data=data)
 
 
-@api.route('/<public_id>')
-@api.param('public_id', 'The User identifier')
-@api.response(404, 'User not found.')
+@api.route("/<public_id>")
+@api.param("public_id", "User identifier")
+@api.response(404, "User not found.")
 class User(Resource):
     """Used for single user related API"""
-    @api.doc('get a user')
     @api.marshal_with(_user)
-    def get(self, public_id):
+    def get(self, public_id: str):
         """
-        Get a user given its identifier
+        Get the User by their Public Id
 
         Args:
-            public_id (str): Public Id of the requested user
+            public_id (str): UUID for the User
 
         Returns:
-            User: Details of the requested User
+            User: Details of the User
         """
         user = get_a_user(public_id)
         if not user:
--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -3,15 +3,26 @@ Module for DTOs used in project APIs
 """
 from flask_restx import Namespace, fields
 
+from .regex import EMAIL_REGEX, PASSWORD_REGEX, UUID_REGEX
+
 
 class UserDto:
     """
     DTO for User related API
     """
-    api = Namespace('user', description='User related operations')
-    user = api.model('user', {
-        'email': fields.String(required=True, description='User e-mail'),
-        'username': fields.String(required=True, description='User username'),
-        'password': fields.String(required=True, description='User password'),
-        'public_id': fields.String(description='User identifier')
+    api = Namespace("user",
+                    description="User related operations")
+    user_input = api.model("user_input", {
+        "email": fields.String(required=True,
+                               description="User e-mail",
+                               pattern=EMAIL_REGEX),
+        "username": fields.String(required=True, description="User username"),
+        "password": fields.String(required=True,
+                                  description="User password",
+                                  min_length=8,
+                                  pattern=PASSWORD_REGEX)
+    })
+    user = api.inherit("user", user_input, {
+        "public_id": fields.String(description="User identifier",
+                                   pattern=UUID_REGEX)
     })
