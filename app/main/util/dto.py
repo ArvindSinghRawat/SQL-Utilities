9d4dda03b757ea492eae209f73833d36a0936044
= <=>
2020-10-03 00:13:50 +0530

    Added DTO for the User's API

---
+++ app/main/util/dto.py
@@ -0,0 +1,17 @@
+"""
+Module for DTOs used in project APIs
+"""
+from flask_restplus import Namespace, fields
+
+
+class UserDto:
+    """
+    DTO for User related API
+    """
+    api = Namespace('user', description='User related operations')
+    user = api.model('user', {
+        'email': fields.String(required=True, description='User e-mail'),
+        'username': fields.String(required=True, description='User username'),
+        'password': fields.String(required=True, description='User password'),
+        'public_id': fields.String(description='User identifier')
+    })
