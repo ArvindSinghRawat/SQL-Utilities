6cdd314789a6b69278ca97d8ca06329c51b95859
= <=>
2020-11-25 18:35:58 +0530

    Made uploads to specific folder

--- app/main/service/file_upload_service.py
+++ app/main/service/file_upload_service.py
@@ -1,7 +1,14 @@
 """
     Business logic for File Upload related operations
 """
+import os
+from datetime import datetime
 from werkzeug.datastructures import FileStorage
+from time import strftime
+
+from ..util.directory_traversal import create_nested_directories
+from ..util import constants
+
 
 def upload_file(file: FileStorage, file_name: str):
     """
@@ -11,4 +18,14 @@ def upload_file(file: FileStorage, file_name: str):
         file (FileStorage): Uploaded File
         file_name (str): Desired name of the saved file, used to uniquely identify the file
     """
-    file.save(file_name)
+    target_dir = find_or_create_static_dir()
+    file.save(os.path.join(target_dir, file_name))
+
+
+def find_or_create_static_dir():
+    source_path = constants.USER_DIRECTORY
+    current_date = datetime.now()
+    parent_dir = current_date.strftime(constants.DIR_FORMAT)
+    source_path = os.path.join(source_path, parent_dir)
+    print(source_path)
+    return create_nested_directories("./", source_path.split('/'))
---
+++ app/main/util/constants.py
@@ -0,0 +1,6 @@
+"""
+    File to expose constants used throughout the project
+"""
+
+USER_DIRECTORY = "app/static/resources/uploads"
+DIR_FORMAT = "%d_%b_%y"
