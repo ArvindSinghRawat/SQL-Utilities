4f1ff6077d1afeda5fea1a37c36db736886287e8
= <=>
2020-11-26 11:12:07 +0530

    Added separate Dto module

---
+++ app/main/dto/upload_file_dto.py
@@ -0,0 +1,13 @@
+"""
+    DTO for APIs related to Upload Files
+"""
+
+from flask_restx import Namespace
+
+
+class UploadDto:
+    """
+    DTO for Upload related API
+    """
+    api = Namespace("upload",
+                    description="Upload related operations")
---
+++ app/main/dto/user_dto.py
@@ -0,0 +1,28 @@
+"""
+    DTOs used in User related APIs
+"""
+from flask_restx import Namespace, fields
+
+from .regex import EMAIL_REGEX, PASSWORD_REGEX, UUID_REGEX
+
+
+class UserDto:
+    """
+    DTO for User related API
+    """
+    api = Namespace("user",
+                    description="User related operations")
+    user_input = api.model("user_input", {
+        "email": fields.String(required=True,
+                               description="User e-mail",
+                               pattern=EMAIL_REGEX),
+        "username": fields.String(required=True, description="User name"),
+        "password": fields.String(required=True,
+                                  description="User password",
+                                  min_length=8,
+                                  pattern=PASSWORD_REGEX)
+    })
+    user = api.inherit("user", user_input, {
+        "public_id": fields.String(description="User identifier",
+                                   pattern=UUID_REGEX)
+    })
