8bca5696fbd02a7b686d502636b0b65e68a388ca
= <=>
2020-11-29 23:33:20 +0530

    Added Common Util for generating UUID

--- app/main/util/common.py
+++ app/main/util/common.py
@@ -2,6 +2,8 @@
 Common utilities for the whole project
 """
 
+from uuid import uuid4
+
 
 def create_response(status: str, message: str) -> dict:
     """
@@ -18,3 +20,7 @@ def create_response(status: str, message: str) -> dict:
         'status': status,
         'message': message
     }
+
+
+def create_random_uuid() -> str:
+    return str(uuid4())
